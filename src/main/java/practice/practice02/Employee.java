c2ad9183360fea051cf2157e40ee792c9d2010c0
吉村 徳人 <n-yosimura@seassist.co.jp>
2024-12-19 20:19:06 +0900

    add Java1-3

---
+++ src/main/java/practice/practice02/Employee.java
@@ -0,0 +1,27 @@
+package practice.practice02;
+
+public class Employee {
+
+	String name;
+	int age;
+
+	public Employee() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
